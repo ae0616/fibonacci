4beeb60d507267421ce37d6a9b1c64b64234c576
Andrew Engelhard <andrew.engelhard@outlook.com>
2018-02-03 19:50:09 -0600

    initial checkin

---
+++ fibtest1.py
@@ -0,0 +1,14 @@
+fibvalues = {
+    0: 1,
+    1: 1,
+}
+
+def fib(n):
+    if n not in fibvalues:
+        fibvalues[n] = fib(n-1) + fib(n-2)
+    return fibvalues[n]
+
+
+if __name__ == '__main__':
+    for i in range(0,20):
+        print(f'fib({i:2}) ==> {fib(i):4}')
