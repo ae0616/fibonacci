c2d16e4bc8262dae12b70517e895b8e23b1ff17c
Andrew Engelhard <andrew.engelhard@outlook.com>
2018-02-03 20:58:26 -0600

    Reformat to PEP standards

--- fibtest1.py
+++ fibtest1.py
@@ -3,12 +3,13 @@ fibvalues = {
     1: 1,
 }
 
+
 def fib(n):
     if n not in fibvalues:
-        fibvalues[n] = fib(n-1) + fib(n-2)
+        fibvalues[n] = fib(n - 1) + fib(n - 2)
     return fibvalues[n]
 
 
 if __name__ == '__main__':
-    for i in range(0,20):
+    for i in range(0, 20):
         print(f'fib({i:2}) ==> {fib(i):4}')
--- fibtest2.py
+++ fibtest2.py
@@ -1,12 +1,9 @@
-
-
-
 def fib(n):
     if n < 2:
         return 1
-    return fib(n-1) + fib(n-2)
+    return fib(n - 1) + fib(n - 2)
 
 
 if __name__ == '__main__':
-    for i in range(0,20):
+    for i in range(0, 20):
         print(f'fib({i:2}) ==> {fib(i):4}')
