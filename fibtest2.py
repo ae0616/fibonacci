06b2e0702dc396de46483cd800f1c88143bb7aca
Andrew Engelhard <andrew.engelhard@outlook.com>
2018-02-03 20:07:30 -0600

    Add simple recursive fibonacci method

---
+++ fibtest2.py
@@ -0,0 +1,12 @@
+
+
+
+def fib(n):
+    if n < 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    for i in range(0,20):
+        print(f'fib({i:2}) ==> {fib(i):4}')
